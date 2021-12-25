611ee06dd4ba1040e9be4f71570b552f732bf171
jeffersons <jeffersonsong@yahoo.com>
2021-12-25 17:22:25 -0500

    Upgrade dse 4.7.0

--- killrvideo-services/src/main/java/com/killrvideo/KillrVideoServices.java
+++ killrvideo-services/src/main/java/com/killrvideo/KillrVideoServices.java
@@ -2,10 +2,13 @@ package com.killrvideo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.cassandra.CassandraAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
 @ComponentScan(basePackages="com.killrvideo")
-@EnableAutoConfiguration
+@EnableAutoConfiguration(exclude = {
+        CassandraAutoConfiguration.class
+})
 public class KillrVideoServices {
 
     /**
