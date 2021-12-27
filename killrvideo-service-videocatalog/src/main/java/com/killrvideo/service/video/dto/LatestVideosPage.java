560c08484cd959da08db7ebd7cfa00c6738e713c
jeffersons <jeffersonsong@yahoo.com>
2021-12-27 10:54:32 -0500

    code refactoring.

--- killrvideo-service-videocatalog/src/main/java/com/killrvideo/service/video/dto/LatestVideosPage.java
+++ killrvideo-service-videocatalog/src/main/java/com/killrvideo/service/video/dto/LatestVideosPage.java
@@ -1,5 +1,8 @@
 package com.killrvideo.service.video.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,6 +11,7 @@ import java.util.List;
  *
  * @author DataStax Developer Advocates team.
  */
+@Getter @Setter
 public class LatestVideosPage {
     /** List of Previews. */
     private List<LatestVideo> listOfPreview = new ArrayList<>();
@@ -27,65 +31,8 @@ public class LatestVideosPage {
     public void addLatestVideos(LatestVideo video) {
         listOfPreview.add(video);
     }
-    
-    /**
-     * Getter for attribute 'listOfPreview'.
-     *
-     * @return
-     *       current value of 'listOfPreview'
-     */
-    public List<LatestVideo> getListOfPreview() {
-        return listOfPreview;
-    }
-    
+
     public int getResultSize() {
         return getListOfPreview().size();
-    }
-
-    /**
-     * Setter for attribute 'listOfPreview'.
-     * @param listOfPreview
-     * 		new value for 'listOfPreview '
-     */
-    public void setListOfPreview(List<LatestVideo> listOfPreview) {
-        this.listOfPreview = listOfPreview;
-    }
-
-    /**
-     * Getter for attribute 'cassandraPagingState'.
-     *
-     * @return
-     *       current value of 'cassandraPagingState'
-     */
-    public String getCassandraPagingState() {
-        return cassandraPagingState;
-    }
-
-    /**
-     * Setter for attribute 'cassandraPagingState'.
-     * @param cassandraPagingState
-     * 		new value for 'cassandraPagingState '
-     */
-    public void setCassandraPagingState(String cassandraPagingState) {
-        this.cassandraPagingState = cassandraPagingState;
-    }
-
-    /**
-     * Getter for attribute 'nextPageState'.
-     *
-     * @return
-     *       current value of 'nextPageState'
-     */
-    public String getNextPageState() {
-        return nextPageState;
-    }
-
-    /**
-     * Setter for attribute 'nextPageState'.
-     * @param nextPageState
-     * 		new value for 'nextPageState '
-     */
-    public void setNextPageState(String nextPageState) {
-        this.nextPageState = nextPageState;
     }
 }
