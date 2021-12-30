bd1f1d58ad98b97ed15e31766bee7eb6751c9c7b
Jefferson Song <jeffersonsong@yahoo.com>
2021-12-30 07:33:37 -0500

    mac config

--- scripts/run-docker-backend-external.sh
+++ scripts/run-docker-backend-external.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # setting environment variable to the IP address of the host
-#export KILLRVIDEO_BACKEND=`ipconfig getifaddr en0`
-#export KILLRVIDEO_DOCKER_IP=`ipconfig getifaddr en0`
-export KILLRVIDEO_BACKEND=$(ip addr show docker0 | grep -Po 'inet \K[\d.]+')
-export KILLRVIDEO_DOCKER_IP=$(ip addr show docker0 | grep -Po 'inet \K[\d.]+')
+export KILLRVIDEO_BACKEND=`ipconfig getifaddr en0`
+export KILLRVIDEO_DOCKER_IP=`ipconfig getifaddr en0`
+#export KILLRVIDEO_BACKEND=$(ip addr show docker0 | grep -Po 'inet \K[\d.]+')
+#export KILLRVIDEO_DOCKER_IP=$(ip addr show docker0 | grep -Po 'inet \K[\d.]+')
 
 # the compose file swaps in the value of `KILLRVIDEO_BACKEND` in several places
 docker-compose -p killrvideo-java -f docker-compose-backend-external.yaml up -d
