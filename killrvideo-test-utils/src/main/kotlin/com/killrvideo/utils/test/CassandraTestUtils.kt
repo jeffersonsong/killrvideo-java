e4c4d676e7e19760c8ea4d0f3adb5e2089a97cba
Jefferson Song <jeffersonsong@yahoo.com>
2021-12-31 14:27:03 -0500

    Convert test-utils to kotlin.

--- killrvideo-test-utils/src/main/kotlin/com/killrvideo/utils/test/CassandraTestUtils.kt
+++ killrvideo-test-utils/src/main/kotlin/com/killrvideo/utils/test/CassandraTestUtils.kt
@@ -1,45 +1,39 @@
-package com.killrvideo.utils.test;
+package com.killrvideo.utils.test
 
-import com.datastax.oss.driver.api.core.MappedAsyncPagingIterable;
-import com.datastax.oss.driver.api.core.cql.Row;
-import com.killrvideo.dse.utils.PageableQuery;
-import com.killrvideo.dse.utils.PageableQueryFactory;
+import com.datastax.oss.driver.api.core.MappedAsyncPagingIterable
+import com.datastax.oss.driver.api.core.cql.Row
+import java.lang.SafeVarargs
+import com.killrvideo.dse.utils.PageableQuery
+import com.killrvideo.dse.utils.PageableQueryFactory
+import org.mockito.ArgumentMatchers.any
+import org.mockito.Mockito.`when`
+import org.mockito.Mockito.mock
+import java.util.*
+import java.util.function.Function
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.function.Function;
-
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-
-public class CassandraTestUtils {
-    @SuppressWarnings("unchecked")
-    public static <T> MappedAsyncPagingIterable<T> mockMappedAsyncPagingIterable(List<T> list) {
-        MappedAsyncPagingIterable<T> iter = mock(MappedAsyncPagingIterable.class);
-        when(iter.hasMorePages()).thenReturn(false);
-        when(iter.currentPage()).thenReturn(list);
-
-        return iter;
+// TODO - Remove JvmStatic, convert to mockk
+object CassandraTestUtils {
+    @JvmStatic
+    fun <T> mockMappedAsyncPagingIterable(list: List<T>?): MappedAsyncPagingIterable<T> {
+        val iter = mock(MappedAsyncPagingIterable::class.java) as MappedAsyncPagingIterable<T>
+        `when`(iter.hasMorePages()).thenReturn(false)
+        `when`(iter.currentPage()).thenReturn(list)
+        return iter
     }
 
-    @SuppressWarnings("unchecked")
+    @JvmStatic
     @SafeVarargs
-    public static <T> PageableQueryFactory mockPageableQueryFactory(PageableQuery<T>... queries) {
-        PageableQueryFactory pageableQueryFactory = mock(PageableQueryFactory.class);
-
-        switch (queries.length) {
-            case 0 :
-                break;
-            case 1:
-                when(pageableQueryFactory.newPageableQuery(any(), any(), (Function<Row, T>) any()))
-                        .thenReturn(queries[0]);
-                break;
-            default:
-                when(pageableQueryFactory.newPageableQuery(any(), any(), (Function<Row, T>) any()))
-                        .thenReturn(queries[0], Arrays.copyOfRange(queries, 1, queries.length));
+    fun <T> mockPageableQueryFactory(vararg queries: PageableQuery<T>?): PageableQueryFactory {
+        val pageableQueryFactory = mock(PageableQueryFactory::class.java)
+        when (queries.size) {
+            0 -> {}
+            1 -> `when`(
+                pageableQueryFactory.newPageableQuery(any(), any(), any<Any>() as Function<Row?, T>?)
+            ).thenReturn(queries[0])
+            else -> `when`(
+                pageableQueryFactory.newPageableQuery(any(), any(), any<Any>() as Function<Row?, T>?)
+            ).thenReturn(queries[0], *Arrays.copyOfRange(queries, 1, queries.size))
         }
-
-        return pageableQueryFactory;
+        return pageableQueryFactory
     }
 }
