0765b53e8481867cf39aea49d305336cddeff1b7
Jefferson Song <jeffersonsong@yahoo.com>
2022-01-04 08:57:33 -0500

    Minimize deps.

--- killrvideo-service-suggestedvideo/build.gradle.kts
+++ killrvideo-service-suggestedvideo/build.gradle.kts
@@ -43,7 +43,6 @@ dependencies {
 
     testImplementation(Deps.Junit.jupiter)
     testImplementation(Deps.mockk)
-    testImplementation(Deps.hamcrest)
 
     kapt(Deps.Datastax.mapperProcessor)
 }
