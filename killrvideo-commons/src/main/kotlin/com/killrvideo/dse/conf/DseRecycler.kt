49f4254ee185e029b6a6b412e77a7971b6062a67
Jefferson Song <jeffersonsong@yahoo.com>
2022-01-06 23:50:21 -0500

    Close Dse before destroy.

---
+++ killrvideo-commons/src/main/kotlin/com/killrvideo/dse/conf/DseRecycler.kt
@@ -0,0 +1,23 @@
+package com.killrvideo.dse.conf
+
+import com.datastax.oss.driver.api.core.CqlSession
+import mu.KotlinLogging
+import org.springframework.stereotype.Component
+import javax.annotation.PreDestroy
+import javax.inject.Inject
+
+@Component
+class DseRecycler {
+    private val logger = KotlinLogging.logger {  }
+    @Inject
+    private lateinit var dseSession: CqlSession
+
+    @PreDestroy
+    fun onDestroy() {
+        if (!dseSession.isClosed()) {
+            logger.info("Closing DSE Cluster (clean up at shutdown)");
+            dseSession.close();
+            logger.info(" + DSE Cluster is now closed.");
+        }
+    }
+}
